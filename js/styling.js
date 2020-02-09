11bf060909c40a7f7544f0bf96c3974a956679d6
Milotrince <trinityjchung@gmail.com>
2020-02-09 02:27:07 -0800

    fix parallax

--- js/styling.js
+++ js/styling.js
@@ -2,12 +2,14 @@ $(".dramatic-list li").each(function() {
     $(this).html($(this).html().replace(/^(\w+)/, '<span class="number">$1</span>'));
 })
 
-$(window).unbind("scroll").scroll(function() {
-    console.log('hi')
-
+function parallax() {
     scrollPercent = $(window).scrollTop() / ($(document).height() - $(window).height());
     $(".block-background").each(function() {
         $(this).css("background-position-y", scrollPercent * 300 + 500 + "px");
     })
+    setTimeout(parallax, 30)
+}
 
+$(document).ready(function() {
+    parallax()
 })
