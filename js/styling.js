cb9f62b84f4f98c7046bae0714ee8f430bc57a87
Milotrince <trinityjchung@gmail.com>
2020-02-09 02:23:13 -0800

    add icons and images

--- js/styling.js
+++ js/styling.js
@@ -1,3 +1,13 @@
 $(".dramatic-list li").each(function() {
     $(this).html($(this).html().replace(/^(\w+)/, '<span class="number">$1</span>'));
 })
+
+$(window).unbind("scroll").scroll(function() {
+    console.log('hi')
+
+    scrollPercent = $(window).scrollTop() / ($(document).height() - $(window).height());
+    $(".block-background").each(function() {
+        $(this).css("background-position-y", scrollPercent * 300 + 500 + "px");
+    })
+
+})
