fb9d9666ed8604f68b5fb8a4e1ae4bac51c6fda6
Milotrince <trinityjchung@gmail.com>
2020-02-09 18:25:51 -0800

    fix for widescreens

--- js/styling.js
+++ js/styling.js
@@ -1,15 +1,32 @@
-$(".dramatic-list li").each(function() {
-    $(this).html($(this).html().replace(/^(\w+)/, '<span class="number">$1</span>'));
-})
+function styleDramaticLists() {
+    $(".dramatic-list li").each(function() {
+        $(this).html($(this).html().replace(/^(\w+)/, '<span class="number">$1</span>'))
+    })
+}
 
 function parallax() {
-    scrollPercent = $(window).scrollTop() / ($(document).height() - $(window).height());
+    scrollPercent = $(window).scrollTop() / ($(document).height() - $(window).height())
+
     $(".block-background").each(function() {
-        $(this).css("background-position-y", scrollPercent * 300 + 500 + "px");
+        $e = $(this)
+        positionY = Math.round(scrollPercent * $e.height()/2 - $e.width() + $e.height()/2)
+        $e.css("background-position-y", positionY + "px")
     })
-    setTimeout(parallax, 30)
+}
+
+var debounceTimer
+function debounce(func) {
+    if (debounceTimer) {
+        window.clearTimeout(debounceTimer)
+    }
+    debounceTimer = window.setTimeout(func, 10)
 }
 
 $(document).ready(function() {
     parallax()
+    styleDramaticLists()
 })
+$(window).scroll(function() {
+    debounce(parallax)
+})
+$(window).resize(parallax)
